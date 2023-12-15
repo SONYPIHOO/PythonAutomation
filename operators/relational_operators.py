ea82107d354655ebc53a31a442493da44c7f27cb
HP <vishaldanawade.it@gmail.com>
2023-12-15 09:01:10 +0530

    Relational Operators

---
+++ operators/relational_operators.py
@@ -0,0 +1,20 @@
+#demonstaration of relational operators
+a=10
+b=4
+
+print("Value of a",a)
+print("Valueof b",b)
+
+print("a>b",a>b)
+print("a<b",a<b)
+print("a>=b",a>=b)
+print("a<=b",a<=b)
+print("a==b",a==b)
+print("a!=b",a!=b)
+
+name="Kavya"
+
+if name=='Kavya':
+    print("Hi "+name+" Good Morning")
+elif name!='Kavya':
+    print("Hello Guest Good Morning")
