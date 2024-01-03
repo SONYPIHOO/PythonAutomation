71182f507e5157d795ffa22770e4d5350647296d
HP <vishaldanawade.it@gmail.com>
2024-01-03 09:47:30 +0530

    usecase eample

--- Python_function/usecase_ex1.py
+++ Python_function/usecase_ex1.py
@@ -2,8 +2,8 @@
 
 import pandas as pd
 
-source=pd.read_csv("Path")
-target=pd.read_csv("Path")
+source=pd.read_csv("PythonAutomation\source_files\Contact_info.csv")
+target=pd.read_csv("PythonAutomation\source_files\Contact_info.csv")
 
 print("SOURCE Details")
 print("Number of rows",source.shape[0])
@@ -21,4 +21,4 @@ def count_validation(sdata,tdata):
     if(scount==tcount):
         print("Count Matched")
     else:
-        print("Count not matched"abs(scount-tcount))
+        print("Count not matched",abs(scount-tcount))
